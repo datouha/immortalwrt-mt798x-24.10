aca1a9b72b3e5e0e6e10e76056dd2aa1f8f6e56c
datouha <13640933252@139.com>
2025-05-19 12:46:45 +0800

    Update diy-part1.sh

--- hanwckf/diy-part1.sh
+++ hanwckf/diy-part1.sh
@@ -22,5 +22,5 @@
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default   
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
