1ecd17dac83ef47210fab678d7cf9439ffbd2be4
datouha <13640933252@139.com>
2025-05-19 14:55:57 +0800

    Update diy-part2.sh

--- Lienol/diy-part2.sh
+++ Lienol/diy-part2.sh
@@ -37,13 +37,13 @@ sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-defaul
 # git clone https://github.com/leshanydy2022/adguardhome.git feeds/packages/net/adguardhome
 
 # 为adguardhome alist passwall插件更换最新的版本
-# rm -rf feeds/packages/net/{chinadns-ng,dns2socks,geoview,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocksr-libev,simple-obfs,sing-box,tcping,trojan-plus,tuic-client,v2ray-geodata,v2ray-plugin,xray-core,xray-plugin}
+rm -rf feeds/packages/net/{chinadns-ng,dns2socks,geoview,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocksr-libev,simple-obfs,sing-box,tcping,trojan-plus,tuic-client,v2ray-geodata,v2ray-plugin,xray-core,xray-plugin}
 # rm -rf feeds/luci/applications/luci-app-passwall 
 # git clone https://github.com/leshanydy2022/luci-app-adguardhome.git package/lean/luci-app-adguardhome
 # git clone https://github.com/leshanydy2022/adguardhome.git feeds/packages/net/adguardhome
-# git clone https://github.com/xiaorouji/openwrt-passwall-packages feeds/luci-app-passwall-packages
-# mv ./feeds/luci-app-passwall-packages/{dns2socks,geoview,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocksr-libev,simple-obfs,sing-box,tcping,trojan-plus,tuic-client,v2ray-geodata,v2ray-plugin,xray-core,xray-plugin,chinadns-ng} ./feeds/packages/net
-# rm -rf feeds/luci-app-passwall-packages
+git clone https://github.com/xiaorouji/openwrt-passwall-packages feeds/luci-app-passwall-packages
+mv ./feeds/luci-app-passwall-packages/{dns2socks,geoview,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocksr-libev,simple-obfs,sing-box,tcping,trojan-plus,tuic-client,v2ray-geodata,v2ray-plugin,xray-core,xray-plugin,chinadns-ng} ./feeds/packages/net
+rm -rf feeds/luci-app-passwall-packages
 # git clone https://github.com/sbwml/luci-app-alist package/alist
 # mv ./package/alist/alist ./feeds/packages/net
 # git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
