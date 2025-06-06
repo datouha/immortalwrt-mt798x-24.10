104abf286c4f2230770f6a2dd57905d523f12529
datouha <13640933252@139.com>
2025-06-06 19:48:14 +0800

    Update diy-part2.sh

--- hanwckf/diy-part2.sh
+++ hanwckf/diy-part2.sh
@@ -28,13 +28,13 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 
 # 安装luci-app-smartdns和luci-app-adguardhome
 # rm -rf feeds/luci/applications/luci-app-smartdns
-# rm -rf feeds/luci/applications/luci-app-adguardhome
+rm -rf feeds/luci/applications/luci-app-adguardhome
 # git clone https://github.com/leshanydy2022/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 git clone https://github.com/leshanydy2022/luci-app-adguardhome.git package/lean/luci-app-adguardhome
 
 # 为adguardhome插件更换最新的版本
-# rm -rf feeds/packages/net/adguardhome
-# git clone https://github.com/leshanydy2022/adguardhome.git feeds/packages/net/adguardhome
+rm -rf feeds/packages/net/adguardhome
+git clone https://github.com/leshanydy2022/adguardhome.git feeds/packages/net/adguardhome
 
 # 为adguardhome alist passwall插件更换最新的版本
 # rm -rf feeds/packages/net/{alist,adguardhome,chinadns-ng,dns2socks,geoview,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocksr-libev,simple-obfs,sing-box,tcping,trojan-plus,tuic-client,v2ray-geodata,v2ray-plugin,xray-core,xray-plugin}
